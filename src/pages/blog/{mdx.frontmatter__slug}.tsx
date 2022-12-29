5f31ddbe1aafe26acab9ab9733349ae0ececf841
tleduc <t.leduc@openium.fr>
2022-12-29 18:17:53 +0100

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -4,13 +4,25 @@ import type {
   HeadFC,
   PageProps,
 } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost: React.FC<PageProps> = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   )
@@ -22,6 +34,14 @@ export const query = graphql`
             frontmatter {
                 title
                 date(formatString: "MMMM D, YYYY")
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
             }
         }
     }
